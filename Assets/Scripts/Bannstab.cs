729e45e3ec29b1322d0c0a9ec9570a156fbf9639
arfe1012 <arfe1012@hs-karlsruhe.de>
2023-05-23 00:06:55 +0200

    Implement Bannstab mechanic v1

--- Assets/Scripts/Bannstab.cs
+++ Assets/Scripts/Bannstab.cs
@@ -12,24 +12,22 @@ public class Bannstab : MonoBehaviour
     private bool candlesAt1Activated = false;
     private bool candlesAt2Activated = false;
     private bool candlesAt3Activated = false;
-    private bool candlesAt4ctivated = false;
+    private bool candlesAt4Activated = false;
     private Vector3 candle0 = new Vector3(-1, 0, (float)-0.65);
     private Vector3 candle1 = new Vector3((float)-1.04, 0, (float)0.6);
-    private Vector3 candle2 = new Vector3((float)0.2, 0, (float)1.105);
-    private Vector3 candle3 = new Vector3(1, 0, (float)0.1);
-    private Vector3 candle4 = new Vector3((float)0.3, 0, -1);
+    private Vector3 candle2 = new Vector3((float)0.3, 0, -1);
+    private Vector3 candle3 = new Vector3((float)0.2, 0, (float)1.105); // 1, 0, (float)0.1
+    private Vector3 candle4 = new Vector3(1, 0, (float)0.1);
+    private int index = 0;
+    private List<Vector3> drawingOrder;
 
     // Start is called before the first frame update
     void Start()
     {
+        drawingOrder = new List<Vector3>();
         Flame1.SetActive(false);
         Flame2.SetActive(false);
-        Vector3[] positions = new Vector3[6];
-        for( int i = 0; i < positions.Length; i++)
-        {
-            positions[i] = candle0;
-        }
-        Line.GetComponent<LineRenderer>().SetPositions(positions);
+        Line.GetComponent<LineRenderer>().positionCount = 0;
     }
 
     // Update is called once per frame
@@ -38,27 +36,42 @@ public class Bannstab : MonoBehaviour
         if (candlesAt0Active() && !candlesAt0Activated)
         {
             candlesAt0Activated = true;
-            Debug.Log("All candles on position 0 lit");
+            Line.GetComponent<LineRenderer>().positionCount = index + 1;
+            Line.GetComponent<LineRenderer>().SetPosition(index, candle0);
+            drawingOrder.Add(candle0);
+            index++;
         }
         if (candlesAt1Active() && !candlesAt1Activated)
         {
             candlesAt1Activated = true;
-            Debug.Log("All candles on position 1 lit");
+            Line.GetComponent<LineRenderer>().positionCount = index + 1;
+            Line.GetComponent<LineRenderer>().SetPosition(index, candle1);
+            drawingOrder.Add(candle1);
+            index++;
         }
         if (candlesAt2Active() && !candlesAt2Activated)
         {
             candlesAt2Activated = true;
-            Debug.Log("All candles on position 2 lit");
+            Line.GetComponent<LineRenderer>().positionCount = index + 1;
+            Line.GetComponent<LineRenderer>().SetPosition(index, candle2);
+            drawingOrder.Add(candle2);
+            index++;
         }
         if (candlesAt3Active() && !candlesAt3Activated)
         {
             candlesAt3Activated = true;
-            Debug.Log("All candles on position 3 lit");
+            Line.GetComponent<LineRenderer>().positionCount = index + 1;
+            Line.GetComponent<LineRenderer>().SetPosition(index, candle3);
+            drawingOrder.Add(candle3);
+            index++;
         }
-        if (candlesAt4Active() && !candlesAt4ctivated)
+        if (candlesAt4Active() && !candlesAt4Activated)
         {
-            candlesAt4ctivated = true;
-            Debug.Log("All candles on position 4 lit");
+            candlesAt4Activated = true;
+            Line.GetComponent<LineRenderer>().positionCount = index + 1;
+            Line.GetComponent<LineRenderer>().SetPosition(index, candle4);
+            drawingOrder.Add(candle4);
+            index++;
         }
     }
 
@@ -72,6 +85,7 @@ public class Bannstab : MonoBehaviour
     {
         Flame1.SetActive(false);
         Flame2.SetActive(false);
+        validateBannkreis();
     }
 
     private bool candlesAt0Active()
@@ -131,5 +145,46 @@ public class Bannstab : MonoBehaviour
         {
             return false;
         }
+    }
+
+    private void validateBannkreis()
+    {
+        if(drawingOrder.Count == 3 && drawingOrder[0].Equals(candle0) && drawingOrder[1].Equals(candle1) && drawingOrder[2].Equals(candle3))
+        {
+            Debug.Log("Correct order");
+
+            /*
+             * 
+             * Set States for Portals in here
+             * 
+             */ 
+        } else
+        {
+            Debug.Log("Wrong order");
+            resetBannkreis();
+        }
+    }
+
+    private void resetBannkreis()
+    {
+        drawingOrder.Clear();
+        Vector3[] resetArray = new Vector3[drawingOrder.Count + 1];
+        for(int i = 0; i <= drawingOrder.Count; i++)
+        {
+            Line.GetComponent<LineRenderer>().SetPosition(i, candle0);
+        }
+        Line.GetComponent<LineRenderer>().positionCount = 0;
+        
+        for(int i = 0; i < flamesOfCandles.Length; i++)
+        {
+            flamesOfCandles[i].transform.GetChild(1).gameObject.SetActive(false);
+        }
+
+        candlesAt0Activated = false;
+        candlesAt1Activated = false;
+        candlesAt2Activated = false;
+        candlesAt3Activated = false;
+        candlesAt4Activated = false;
+        index = 0;
     }
 }
