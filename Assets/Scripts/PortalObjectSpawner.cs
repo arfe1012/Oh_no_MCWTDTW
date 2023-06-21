02728490f30c98e8f982b12d09d7235025b67bf5
JohannesOtt <otjo1012@hs-karlsruhe.de>
2023-06-22 01:01:18 +0200

    Put PocketDimension in MainScene, Put Neues Portal in MainScene

--- Assets/Scripts/PortalObjectSpawner.cs
+++ Assets/Scripts/PortalObjectSpawner.cs
@@ -6,45 +6,92 @@ using UnityEngine;
 public class PortalObjectSpawner : MonoBehaviour
 {
     public GameObject PortalShape;
-    public bool spawnShape;
     [SerializeField] private Material material;
+    [SerializeField] private Material ObSpMaterial;
 
-    Vector3 newScale = new Vector3(1, 1, 1);
+    public GameObject prefab1;
+    public GameObject prefab2;
+    public GameObject prefab3;
 
-
-
+    GameObject prefabToSpawn;
 
     private void Start()
     {
+        material.SetFloat("_DissolveAmount", 0);
+        ObSpMaterial.SetFloat("_DissolveAmount", 0);
+        SpawnPrefab1();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (spawnShape)
-        {
-            StartCoroutine(SpawnPortalCoroutine());
-            spawnShape = false;
-        }
-
-
-
+        
     }
+
+    void SpawnPrefab1()
+    {
+        prefabToSpawn = prefab1;
+        StartCoroutine(SpawnPortalCoroutine());
+    }
+
+    void SpawnPrefab2()
+    {
+        prefabToSpawn = prefab1;
+        StartCoroutine(SpawnPortalCoroutine());
+    }
+
+    void SpawnPrefab3()
+    {
+        prefabToSpawn = prefab1;
+        StartCoroutine(SpawnPortalCoroutine());
+    }
+
+
     IEnumerator SpawnPortalCoroutine()
 
     {
+        material.SetFloat("_DissolveAmount", 1);
+        for (float dissolve = 0; dissolve <= 1; dissolve += Time.deltaTime) //Macht die PocketDim größer
+        {
+             transform.localScale= new Vector3(dissolve, dissolve, dissolve);
+            yield return null;
+        }
 
         for (float dissolve = 0; dissolve <= 1; dissolve += Time.deltaTime) //Baut den Swoosh Shader auf
         {
-            
-            material.SetFloat("_DissolveAmount", dissolve);
+            ObSpMaterial.SetFloat("_DissolveAmount", dissolve);
             yield return null;
-            if (UnityEngine.Random.value >= 0.9)
-            {
-                transform.Rotate(1, -1, 1, Space.Self);
-            }
         }
 
+        SpawnObject(); //Spawnt das Gewünschte Objekt
+
+        for (float dissolve = 1; dissolve >= 0; dissolve -= Time.deltaTime) //Baut den Swoosh Shader ab
+        {
+            ObSpMaterial.SetFloat("_DissolveAmount", dissolve);
+            yield return null;
+        }
+
+
+        
+        yield return new WaitForSeconds(5); // Verzögerung von 5 Sekunden
+        for (float dissolve = 1; dissolve >= 0; dissolve -= Time.deltaTime) //Baut den Swoosh Shader ab
+        {
+            material.SetFloat("_DissolveAmount", dissolve);
+            yield return null;
+        }
     }
 
- }
+
+    void SpawnObject()
+    {
+        Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+    }
+
+
+    
+
+    
+
+}
