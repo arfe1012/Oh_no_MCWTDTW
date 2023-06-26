36dd9b1f4a7fe5f469a57a420d37ae7c00d93798
DayVe43 <mida1011@hs-karlsruhe.de>
2023-06-26 12:40:01 +0200

    FIXED small things, and add Build

--- Assets/Scripts/SwitchSceneToMain.cs
+++ Assets/Scripts/SwitchSceneToMain.cs
@@ -7,6 +7,6 @@ public class SwitchSceneToMain : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 }
